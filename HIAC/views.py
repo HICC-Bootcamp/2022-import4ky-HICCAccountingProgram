65474672d824556fdd2bdb503ea5a3794f5d073f
DESKTOP-A1PJ2SV\boy81 <boy814@naver.com>
2022-07-22 19:03:31 +0900

    엑셀파일 잠금해제 함수, 엑셀 읽어오는 함수, 특정 행을 읽어오는 함수, 특정 열을 읽어오는 함수

--- HIAC/views.py
+++ HIAC/views.py
@@ -1,3 +1,53 @@
 from django.shortcuts import render
 
+import msoffcrypto
+import pathlib
+import pandas as pd
+
 # Create your views here.
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
+
+def unlock_main(password):
+    url = pathlib.Path(r'.\xlsx')
+    excel_files = list(url.glob('*.xlsx'))
+    for i in excel_files:
+        unlock(i, password, r'.\xlsx\xlsx2')
+
+def unlock(file_name, passwd, output_folder):
+    temp = open(file_name, 'rb')
+    excel = msoffcrypto.OfficeFile(temp)
+    excel.load_key(passwd)
+    out_path = pathlib.Path(output_folder)
+    if not out_path.exists():
+        out_path.mkdir()
+
+    with open(str(out_path/file_name.name), 'wb') as f:
+        excel.decrypt(f)
+    temp.close()
+
+def read_table(): # 필요한 테이블 가져오는 함수
+    url = pathlib.Path(r'.\xlsx\xlsx2')
+    excel_files = list(url.glob('*.xlsx'))
+    df=pd.read_excel(excel_files[0], header=10, usecols=[1, 2, 3, 4, 6, 7], engine='openpyxl')
+    print(df)
+    return df
+
+
+def extract_rows(table, row_list): # 원하는 열(가로줄)의 정보를 가져오는 함수
+    return table.loc[row_list]
+
+
+def extract_cols(table, col_list): # 원하는 행(새로줄)의 정보를 가져오는 함수
+    return table[col_list]
+
+#unlock_main('000814')
+
+#row_list=[1, 2, 3, 4]
+#col_list=['거래일시']
+table=read_table()
+#print(extract_rows(table, row_list))
+#print(extract_cols(table, col_list))
+print(extract_cols(table, '내용'))
+len(extract_cols(table, '내용'))
+
