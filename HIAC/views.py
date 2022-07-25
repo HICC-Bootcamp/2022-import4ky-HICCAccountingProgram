4838603717d8f5669ee6c30ad8607ca95ed39d71
김진호 <rlawlsgh1227@gmail.com>
2022-07-25 14:24:43 +0900

    unlock함수 사용해보려 했지만 아직 인식하지 못하는 이슈발생

--- HIAC/views.py
+++ HIAC/views.py
@@ -7,23 +7,29 @@ import pandas as pd
 import numpy as np
 
 # Create your views here.
-leftTable=pd.DataFrame({'거래일시':[],'거래금액':[], '내용':[],'메모':[]})
-rightTable=pd.DataFrame({'거래일시':[],'거래금액':[], '내용':[],'메모':[]})
+leftTable = pd.DataFrame({'거래일시': [], '거래금액': [], '내용': [], '메모': []})
+rightTable = pd.DataFrame({'거래일시': [], '거래금액': [], '내용': [], '메모': []})
+
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
 
 
 def intro(request):
     context = {}
-    if request.method == 'POST':
+    if request.method == 'POST' and 'excel' in request.FILES:
         uploaded_file = request.FILES['excel']
         fs = FileSystemStorage()
         name = fs.save(uploaded_file.name, uploaded_file)
-        context['url'] = fs.url(name)
+        context = {
+            'url': fs.url(name),
+        }
+
+    if request.method == "POST" and 'password' in request.POST:
+        pw = request.POST.get('password')
+        unlock_main(pw)
+        print("password 해제 완료!")
 
     return render(request, 'HIAC/intro.html', context)
-
-
-pd.set_option('display.max_columns', None)
-pd.set_option('display.max_rows', None)
 
 
 def unlock_main(password):
@@ -46,7 +52,7 @@ def unlock(file_name, passwd, output_folder):
     temp.close()
 
 
-def read_table(): # 필요한 테이블 가져오는 함수
+def read_table(): # 필요한 테이블 가져 오는 함수
     url = pathlib.Path(r'./xlsx/xlsx2')
     excel_files = list(url.glob('*.xlsx'))
     df = pd.read_excel(excel_files[0], header=10, usecols=[1, 3, 6, 7], engine='openpyxl')
@@ -54,17 +60,19 @@ def read_table(): # 필요한 테이블 가져오는 함수
     return df
 
 
-def extract_rows(table, row_list): # 원하는 열(가로줄)의 정보를 가져오는 함수
+def extract_rows(table, row_list): # 원하는 열(가로줄)의 정보를 가져 오는 함수
     return table.loc[row_list]
 
 
-def extract_cols(table, col_list): # 원하는 행(새로줄)의 정보를 가져오는 함수
+def extract_cols(table, col_list): # 원하는 행(새로줄)의 정보를 가져 오는 함수
     return table[col_list]
+
 
 #회계 정보 페이지
 def readExel():
     global leftTable
     leftTable = read_table()
+
 
 def moveRight(data):
     global rightTable
@@ -73,12 +81,14 @@ def moveRight(data):
     rightTable=rightTable.sort_values(by='거래일시')
     print(rightTable)
 
+
 def deleteOverlap():
     global rightTable
     rightTable = rightTable.drop_duplicates()
     print(rightTable)
 
-#unlock_main('981227')
+
+unlock_main('981227')
 
 #회계정보페이지 test
 #row_list=[1, 2, 3, 4]
@@ -89,9 +99,9 @@ def deleteOverlap():
 #print(extract_cols(table, col_list))
 #print(extract_cols(table, '내용'))
 #len(extract_cols(table, '내용'))
-readExel()
-data={'거래일시':'2022.06.30 23:33:34','거래금액':'-370,500','내용':'어른이대공원(본점)','메모' : np.nan}
-rightTable = leftTable
-moveRight(data)
-deleteOverlap()
+#readExel()
+#data={'거래일시':'2022.06.30 23:33:34','거래금액':'-370,500','내용':'어른이대공원(본점)','메모' : np.nan}
+#rightTable = leftTable
+#moveRight(data)
+#deleteOverlap()
 #print(leftTable)
